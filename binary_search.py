f2d91e722f0ea12307487e49eac50084a74b32cb
Michael Spagon <mspagon@gmail.com>
2024-11-08 20:19:00 -0600

    Create binary_search.py
    initial commit

---
+++ binary_search.py
@@ -0,0 +1,53 @@
+def binary_search(a, x, lo=0, hi=None):
+    if lo < 0:
+        raise ValueError('lo must be non-negative')
+    if hi is None:
+        hi = len(a)
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if a[mid] == x:
+            return mid
+        elif a[mid] < x:
+            lo = mid + 1
+        else:
+            hi = mid - 1
+    return -1
+
+
+def bisect_right(a, x, lo=0, hi=None):
+    if lo < 0:
+        raise ValueError('lo must be non-negative')
+    if hi is None:
+        hi = len(a)
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if x < a[mid]:
+            hi = mid
+        else:
+            lo = mid + 1
+    return lo
+
+
+def bisect_left(a, x, lo=0, hi=None):
+    if lo < 0:
+        raise ValueError('lo must be non-negative')
+    if hi is None:
+        hi = len(a)
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if a[mid] < x:
+            lo = mid + 1
+        else:
+            hi = mid
+    return lo
+
+
+def main():
+    nums = [0, 1, 5, 8, 8, 8, 8, 8, 10, 22, 37, 41, 55]
+    print(binary_search(nums, 8))
+    print(bisect_left(nums, 8))
+    print(bisect_right(nums, 8))
+
+
+if __name__ == '__main__':
+    main()
